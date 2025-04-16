686c586df52083aa7e346746efa4902b1f92691d
Pertti P <PerttiP@users.noreply.github.com>
2025-04-16 20:26:24 +0200

    Simplified Get Runner code

--- ASSETS/ExtraFunctions/oktyrFunctions.js
+++ ASSETS/ExtraFunctions/oktyrFunctions.js
@@ -39,36 +39,3 @@ function SPX_UI_popup(message) {
     // A basic message to OPERATOR in SPX UI
     alert(message);
 }
-
-function getRunner() {
-    //alert('getRunner() CALLED!');
-
-    // Note: The prompt() function only works in client browsers, not in Node.js server-side!
-    let input = prompt("Ange nummerlapps-nummer (1-999):", "0");
-    if (input !== null) {
-        let number;        
-        try {
-            number = parseInt(input); // Convert to integer
-        }
-        catch {
-            console.error("Invalid number entered. Failed to parse as Int");
-        }       
-        
-        if (!isNaN(number)) {
-            // Use the integer value as needed
-            console.log("The number entered is:", number); // OK SO FAR!
-            window.TyrAppGlobals.startNumber = number;
-
-            // TODO:
-            // Can we initiate the fetching of runner data from here?
-            //getRunnerData(number);
-            window.TyrAppGlobals.getRunnerData(number);
-
-        } else {
-            console.log("Invalid number entered. Please add another number (1-999)");
-            alert('Numret var felaktigt. Försök med annat nummer (1-999)');
-            //FIXME:
-            //Should we keep the previous start number?
-        }
-    }
-}
--- ASSETS/templates/oktyr/sm2025medeldist/js/spx_interface.js
+++ ASSETS/templates/oktyr/sm2025medeldist/js/spx_interface.js
@@ -2,6 +2,10 @@
 // (c) Copyright 2021- SPX Graphics (https://spx.graphics)
 // ----------------------------------------------------------------
 
+//
+//  Ensure spx_interface.js is responsible for all DOM updates and interactions with window.SPXGCTemplateDefinition.
+//
+
 
 // TEST: Global Window variables
 // -----------------------------
@@ -14,11 +18,14 @@ window.TyrAppGlobals = window.TyrAppGlobals || {};
 if (window.TyrAppGlobals.startNumber === undefined) {
   window.TyrAppGlobals.startNumber = 0;
 }
+if (window.TyrAppGlobals.jsonRunnerInfoData === undefined) {
+  window.TyrAppGlobals.jsonRunnerInfoData = {}; // This will hold the fetched JSON data
+}
 
 
+// Update the DOM and SPXGCTemplateDefinition here!
 // Receive item data from SPX Graphics Controller (field editor)
-// and store values in hidden DOM elements for
-// use in the template.
+// and store values in hidden DOM elements for use in the template.
 
 function update(data) {
   var templateData = JSON.parse(data);
@@ -38,11 +45,13 @@ function update(data) {
         idField.innerText = '';
       }
     } else {
-      // Pertti: Seems to never enter the else branch?
+      //alert('update(data) - IN ELSE BRANCH!');
+      // Pertti: Enter else branch here if NOT found the field in DOM...
+      // We do enter this else branch when PLAY clicked (and fields changed?)
       switch (dataField) {
         case 'comment':
         case 'epochID':
-          // console.warn('FYI: Optional #' + dataField + ' missing from SPX template...');
+          console.warn('FYI: Optional #' + dataField + ' missing from SPX template...');
           break;
           //FIXME: What can these be used for?
           case 'f_list_titel':
@@ -51,11 +60,26 @@ function update(data) {
           case 'f_vald_klass':
             //alert('Update handler with f_vald klass');
             break;
+          case 'f0':
+            alert('Update handler with missing f0');
+            break;
+          case 'f1':
+            alert('Update handler with missing f1');
+            break;
+          case 'f2':
+            alert('Update handler with missing f2');
+            break;
+          case 'f3':
+            alert('Update handler with missing f3');
+            break;
+          
         default:
           console.error('ERROR Placeholder #' + dataField + ' missing from SPX template.');
       }
     }
   }
+
+  console.log('NOW CALLING runTemplateUpdate() - Play will follow?');
 
   // Once DOM updated we can initPageData and run animations...
   if (typeof runTemplateUpdate === "function") { 
@@ -81,37 +105,91 @@ function formatJSONAndSetDomFields(fieldData) {
   }
 }
 
+function checkWindowTyrAppGlobals() {
+  // Check if window.TyrAppGlobals.jsonRunnerInfoData is defined and is an object
+  if (!window.TyrAppGlobals || !window.TyrAppGlobals.jsonRunnerInfoData || typeof window.TyrAppGlobals.jsonRunnerInfoData !== 'object') {
+    console.error('window.TyrAppGlobals.jsonRunnerInfoData is not defined or not an object');
+    return false;
+  }
+  return true;
+}
+
+function findMatchingRunner(startNumberStr) {
+  if (!checkWindowTyrAppGlobals) { return null; }
+
+  // Get the list of entries from jsonRunnerInfoData
+  const runnerInfoEntries = Object.entries(window.TyrAppGlobals.jsonRunnerInfoData);
+
+  console.log(typeof startNumberStr);  
+
+  // Iterate through each entry
+  for (const [key, fields] of runnerInfoEntries) {
+
+    console.log(typeof fields);
+
+      // Search for a field where title is "Number" and value matches startNumberStr
+      const matchingField = fields.find(field => {
+          console.log(typeof field.value);
+          return field.title === "Number" && field.value === startNumberStr;
+      });
+
+      // If a matching field is found, return the key and fields
+      if (matchingField) {
+          return { key, fields };
+      }
+  }
+
+  // If no matching runner is found, return null
+  return null;
+}
+
 // This getRunnerData function is triggered by extra button 'Get Runners'!
 function getRunnerData(startNumber) {
-  console.log('----- getRunnerData called with startNumber:', startNumber)
+  const item = findMatchingRunner(startNumber.toString());
+  if (item) {
+      console.log(item);
+      //FIXME:
+      //ALT 1: Is it now we can call update() function above???
+      update(item.JSON);
 
-  let item = {};
-  // Get the ONE and ONLY matching item from jsonRunnerInfoData
-  for (const [key, fields] of Object.entries(window.TyrAppGlobals.jsonRunnerInfoData)) {
-    const numberField = fields.find(
-      field => field.title === "Number" && field.value === startNumber
-    );
-    if (numberField) {
-      item = { key, fields }; // Return the key and the matching item array
+      //ALT2: Or do we need to do sth more???
+  }
+  else {
+    alert('Numret kunde inte hittas.');
+  }
+}
 
-      if (item) {
-        //FIXME:
-        //ALT 1: Is it now we can call update() function above???
-        update(item.JSON);
+// Custom Controls Button event handler
+function getRunner() {
+  //alert('getRunner() CALLED!'); //OK ALSO FROM /templates/oktyr/.../js/spx_interface.js folder!
 
-        //ALT2: Or do we need to do sth more???
-
-        // Once DOM updated we can set the fields in the SPX UI template def?
-        /*
-        if (typeof runTemplateUpdate === "function") { 
-          runTemplateUpdate() // Play will follow
-        } else {
-          console.error('runTemplateUpdate() function missing from SPX template.')
-        }
-        */
+  // Note: The prompt() function only works in client browsers, not in Node.js server-side!
+  let input = prompt("Ange nummerlapps-nummer (1-999):", "0");
+  if (input !== null) {
+      let number;        
+      try {
+          number = parseInt(input); // Convert to integer
       }
-    }
-  }  
+      catch {
+          console.error("Invalid number entered. Failed to parse as Int");
+      }       
+      
+      if (!isNaN(number)) {
+          // Use the integer value as needed
+          console.log("The number entered is:", number); // OK SO FAR!
+          window.TyrAppGlobals.startNumber = number;
+
+          // TODO:
+          // Can we initiate the fetching of runner data from here?
+          getRunnerData(number);
+          //window.TyrAppGlobals.getRunnerData(number);
+
+      } else {
+          console.log("Invalid number entered. Please add another number (1-999)");
+          alert('Numret var felaktigt. Försök med annat nummer (1-999)');          
+          // We keep the previous start number?
+      }
+  }
 }
 
 // Play handler
