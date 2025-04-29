edf966c50f7c650f027cb3edf415fe99a997e6fd
Pertti P <PerttiP@users.noreply.github.com>
2025-04-29 08:47:48 +0200

    get selected data from template (issue with function e)

--- ASSETS/templates/oktyr/sm2025medeldist/js/spx_interface.js
+++ ASSETS/templates/oktyr/sm2025medeldist/js/spx_interface.js
@@ -6,7 +6,11 @@
 
 let selectedClass;
 let selectedRunnerBib;
+let validRunnerSelectedInUI = true;
 
+console.log("!!!! NOTE: This spx_interface.js script MUST EXECUTE FIRST !!!!");
+
+// TODO: Could we use local storage as a fallback to get 'previous' selected Runner's data?
 function getDataFromLocalStorage() {
   selectedClass = localStorage.getItem("selectedClass");
   selectedRunnerBib = localStorage.getItem("selectedRunnerBib");
@@ -34,12 +38,9 @@ function followSelectedRunner() {
 
 // Editor button click handler
 function updateFollowedRunner() {
-  alert("updateFollowedRunner() CALLED!");
+  //alert("updateFollowedRunner() CALLED!");
 
-  if (!getDataFromLocalStorage()) {
-    console.warn("Failed to get Runner data from local storage");
-    return;
-  }
+  getSelectedDataFromTemplate();
 
   // HARD-CODED MOCKTEST 2025-04-24:
   const mockData = {
@@ -57,10 +58,6 @@ function updateFollowedRunner() {
       },
     ],
   };
-}
-
-function getSelectedRunnersName() {
-  return "Testar om namn kan ändras i en caption field"; // VERKAR FUNKA EN GÅNG VID INIT AV HTML MALL
 }
 
 // Receive item data from SPX Graphics Controller
@@ -92,6 +89,9 @@ function update(data) {
       }
     }
   }
+
+  // TODO: Check that we have retrieved a valid runner from API
+  let validRunnerFoundFromAPI = true;
 
   if (typeof runTemplateUpdate === "function") {
     runTemplateUpdate(); // Play will follow
@@ -175,3 +175,83 @@ function validString(str) {
   }
   return true; // is a valid string
 }
+
+function getSelectedDataFromTemplate() {
+  e("vald_klass").innerHTML = htmlDecode(e("f_vald_klass").innerText);
+
+  // If selected a bib from dropdown
+  if (e("f_vald_runner_bib").innerText) {
+    e("vald_runner_bib").innerHTML = htmlDecode(
+      e("f_vald_runner_bib").innerText
+    );
+  } else {
+    alert(
+      "No runner selected! Trying to use runner id from input text field.\n Note that you may select a runner in dropdown\n."
+    );
+
+    // If not valid runner selected, then check if user has entered a bib number into input control?
+    if (e("f0").innerText !== "") {
+      // TODO: Check that it is a number!
+      e("vald_runner_bib").innerHTML = htmlDecode(e("f0").innerText);
+
+      validRunnerSelectedInUI = true;
+    } else {
+      alert(
+        "No runner selected! Please either select a runner in dropdown\n or write a valid runner id in input text field."
+      );
+      validRunnerSelectedInUI = false;
+      return false; // <----------- RETURN!
+    }
+  }
+
+  // Get the vald klass from dropdown
+  // NOTE: The value property is only available for form elements
+  // like <input>, <textarea>, or <select>.
+  let element = document.getElementById("f_vald_klass");
+  console.log(element);
+
+  if (typeof element != "undefined" && element != null) {
+    // NOTE: Use innerText to get the content of the div.
+    selectedClass = element.innerText;
+
+    // Save persistently
+    if (selectedClass) {
+      localStorage.setItem("selectedClass", selectedClass);
+      console.log("selectedClass saved: ", selectedClass);
+    }
+  }
+
+  // First try to get the bib number from dropdown
+  let elem_runner = document.getElementById("f_vald_runner");
+
+  if (typeof elem_runner != "undefined" && elem_runner != null) {
+    // NOTE: Use innerText to get the content of the div.
+    selectedRunnerBib = elem_runner.innerText;
+
+    // Save persistently
+    if (selectedRunnerBib) {
+      localStorage.setItem("selectedRunnerBib", selectedRunnerBib);
+      console.log("selectedRunnerBib saved: ", selectedRunnerBib);
+    } else {
+      validRunnerSelectedInUI = false;
+    }
+  }
+  if (!validRunnerSelectedInUI) {
+    // Try to get the runner bib number from editable input textfield instead
+    selectedRunnerBib = htmlDecode(e("f0").innerText);
+    //alert(selectedRunnerBib);
+    if (selectedRunnerBib) {
+      localStorage.setItem("selectedRunnerBib", selectedRunnerBib);
+      console.log("BibNr saved", selectedRunnerBib);
+    } else {
+      console.error("Failed to get runner bib number from editor controls");
+      return false; // <----------- RETURN!
+    }
+  }
+
+  return true;
+}
+
+// ----------------------------------------------------------------
+
+console.log("!!!! Now spx_interface.js script has FINISHED !!!!");
